d79db56b1bc31beb6541b77697332aeee520a1fa
Bogdan Chadkin <trysound@yandex.ru>
2015-11-07 16:38:13 +0300

    Replace mocha tests with ava

--- test/index.js
+++ test/index.js
@@ -1,22 +1,20 @@
-var assert = require('assert');
-var rollup = require('rollup');
-var string = require('..');
+import test from 'ava';
+import { rollup } from 'rollup';
+import string from '..';
 
 process.chdir(__dirname);
 
-describe('rollup-plugin-string', function (t) {
-	it('should import string', function () {
-		return rollup.rollup({
-			entry: 'samples/basic/main.js',
-			plugins: [
-				string({
-					extensions: ['.html', '.css']
-				})
-			]
-		}).then(function (bundle) {
-			var code = bundle.generate().code;
-			var fn = new Function('assert', code);
-			fn(assert);
-		});
+test(t => {
+	return rollup({
+		entry: 'samples/basic/main.js',
+		plugins: [
+			string({
+				extensions: ['.html', '.css']
+			})
+		]
+	}).then(bundle => {
+		var code = bundle.generate().code;
+		var fn = new Function('assert', code);
+		fn(t);
 	});
 });
--- test/samples/basic/main.js
+++ test/samples/basic/main.js
@@ -1,6 +1,6 @@
 import tpl from './tpl.html';
 
-assert.equal(tpl, [
+assert.is(tpl.replace(/\r\n/g, '\n'), [
 	'<section class="section">',
 	'	<article class=\'article\'>Article 1</article>',
 	'	<article class=\'article\'>Article 2</article>',
