b063aa4aa3c530a93b20785f4bee997b87f1c9d2
Bogdan Chadkin <trysound@yandex.ru>
2015-11-20 12:38:44 +0300

    Update deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import babel from 'rollup-plugin-babel';
 
 export default {
 	entry: 'src/index.js',
-	plugins: [babel()],
+	plugins: [babel({
+		presets: ['es2015-rollup']
+	})],
 	format: 'cjs',
 	dest: 'dist/index.js'
 };
