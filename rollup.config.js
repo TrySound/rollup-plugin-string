a7348e455bd4f8a8e4f0f860e382a9deb2da82d5
Bogdan Chadkin <trysound@yandex.ru>
2016-06-12 22:57:32 +0300

    Simpler name dest

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import buble from 'rollup-plugin-buble';
 
+var pkg = require('./package.json');
+
 export default {
 	entry: 'index.js',
 	plugins: [buble()],
 	targets: [
 		{
 			format: 'cjs',
-			dest: 'dist/rollup-plugin-string.js'
+			dest: pkg['main']
 		},
 		{
 			format: 'es6',
-			dest: 'dist/rollup-plugin-string.mjs'
+			dest: pkg['jsnext:main']
 		}
 	]
 };
