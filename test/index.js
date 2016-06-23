6c1551d1c719800b020ca40d28588fff9d526d2a
Bogdan Chadkin <trysound@yandex.ru>
2016-06-23 21:50:02 +0300

    Add test with sourcemaps

--- test/index.js
+++ test/index.js
@@ -4,29 +4,30 @@ var string = require('../');
 
 process.chdir('test');
 
-function makeBundle(entry, options) {
-	return rollup({
-		entry,
-		plugins: [string(options)]
-	});
+function makeBundle(options, stringOptions) {
+	options.plugins = [string(stringOptions)];
+	return rollup(options);
 }
 
 describe('rollup-plugin-string', () => {
 	it('should stringify importing template', () => {
-		return makeBundle('fixtures/basic.js', {
-			include: '**/*.html'
-		}).then(bundle => {
-			const { code } = bundle.generate({
-				format: 'iife',
-				moduleName: 'tpl'
-			});
+		return makeBundle({ entry: 'fixtures/basic.js' }, { include: '**/*.html' }).then(bundle => {
+			const { code } = bundle.generate({ format: 'iife', moduleName: 'tpl' });
 			new Function('assert', code)(assert);
+		});
+	});
+
+	it('should output empty sourcemap', () => {
+		return makeBundle({ entry: 'fixtures/basic.js' }, { include: '**/*.html' }).then(bundle => {
+			const { code, map } = bundle.generate({ sourceMap: true });
+			assert.ok(code);
+			assert.ok(map);
 		});
 	});
 
 	it('throws when include is not specified', () => {
 		assert.throws(() => {
-			makeBundle('fixtures/basic.js')
+			makeBundle({ entry: 'fixtures/basic.js' });
 		}, /include option should be specified/);
 	});
 });
