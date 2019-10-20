b58326737646532a79b892fd809e747abe3e3e72
munrocket <munrocket@pm.me>
2019-10-20 21:30:37 +0400

    add string modify

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ function string(opts = {}) {
     throw Error("include option should be specified");
   }
 
-  const filter = createFilter(opts.include, opts.exclude);
+  const filter = createFilter(opts.include, opts.exclude, opts.modify);
 
   return {
     name: "string",
 
     transform(code, id) {
       if (filter(id)) {
+        code = JSON.stringify(code);
+        code = opts.modify ? opts.modify(code) : code;
         return {
-          code: `export default ${JSON.stringify(code)};`,
+          code: `export default ${code};`,
           map: { mappings: "" }
         };
       }
